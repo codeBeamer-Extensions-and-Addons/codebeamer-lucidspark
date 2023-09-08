25a43a4212d49365584cbafef026191ce6ac00b8
Ethan Baumgartner <ethan.baumgartner@roche.com>
2023-09-08 09:32:06 +0200

    Revert "read block creation documentation, not working"
    This reverts commit 813751b33e59ed8c1c6e487b8da69536c5269054.

--- editorextensions/codebeamer-cards/src/extension.ts
+++ editorextensions/codebeamer-cards/src/extension.ts
@@ -1,42 +1,9 @@
 import {
 	EditorClient,
 	LucidCardIntegrationRegistry,
-	Menu,
-	MenuType,
-	PageProxy,
 } from 'lucid-extension-sdk';
 import { CodeBeamerCardIntegration } from './codebeamercardintegration';
 const client = new EditorClient();
-
-function createProcessBlock(page: PageProxy, x: number, y: number) {
-	const block = page.addBlock({
-		className: 'ProcessBlock',
-		boundingBox: {
-			x,
-			y,
-			w: 200,
-			h: 160,
-		},
-	});
-	block.textAreas.set('Text', 'The new shape');
-}
-
-async function init() {
-	await client.loadBlockClasses(['ProcessBlock']);
-
-	const menu = new Menu(client);
-	menu.addMenuItem({
-		label: 'Test thing 2',
-		action: 'test',
-		menuType: MenuType.Main,
-	});
-
-	// create a block
-	const page = new PageProxy('test', client);
-	createProcessBlock(page, 0, 0);
-}
-
-init();
 
 LucidCardIntegrationRegistry.addCardIntegration(
 	client,
