47ec095e6af3912085c253eb4224d7e37fe5cb2c
Ethan Baumgartner <ethan.baumgartner@roche.com>
2023-09-26 11:23:07 +0200

    removed unused editor extensions

--- editorextensions/codebeamer-cards/resources/resource.d.ts
+++
@@ -1,9 +0,0 @@
-declare module '*.html' {
-    const content: string;
-    export default content;
-}
-
-declare module '*.json' {
-    const content: Record<string, string>;
-    export default content;
-}
