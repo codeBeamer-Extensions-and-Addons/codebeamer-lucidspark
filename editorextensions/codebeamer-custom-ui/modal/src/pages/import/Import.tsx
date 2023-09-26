a06736a205a905797b7ea41880c7ecc1d4f6c28c
Ethan Baumgartner <ethan.baumgartner@roche.com>
2023-09-26 09:18:25 +0200

    removed import header

--- editorextensions/codebeamer-custom-ui/modal/src/pages/import/Import.tsx
+++ editorextensions/codebeamer-custom-ui/modal/src/pages/import/Import.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import ImportHeader from './components/importHeader/ImportHeader';
 import Query from './components/query/Query';
 import QueryResults from './components/queryResults/QueryResults';
 
@@ -8,7 +7,6 @@ import './import.css';
 export default function Import() {
 	return (
 		<div className="fade-in import" data-test="import">
-			<ImportHeader />
 			<Query />
 			<QueryResults />
 		</div>
