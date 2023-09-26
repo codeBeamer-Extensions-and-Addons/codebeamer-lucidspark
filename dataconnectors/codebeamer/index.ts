f876d1dbbe575ca95089af903bff9d05045d67e1
Ethan Baumgartner <ethan.baumgartner@roche.com>
2023-09-26 11:13:43 +0200

    removed unused data connector

--- dataconnectors/codebeamer/index.ts
+++
@@ -1,9 +0,0 @@
-import { DataConnector, DataConnectorClient } from 'lucid-extension-sdk';
-import { importAction } from './actions/import';
-import { DataAction } from '../../common/names';
-
-export const makeDataConnector = (client: DataConnectorClient) =>
-	new DataConnector(client).defineAsynchronousAction(
-		DataAction.Import,
-		importAction
-	);
