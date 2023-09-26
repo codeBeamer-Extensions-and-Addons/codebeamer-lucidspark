ba33839f92a9f726aadc51eb78abdb9c543cf8f9
Ethan Baumgartner <ethan.baumgartner@roche.com>
2023-09-26 10:16:07 +0200

    created footer with header buttons

--- editorextensions/codebeamer-custom-ui/modal/src/pages/import/Import.tsx
+++ editorextensions/codebeamer-custom-ui/modal/src/pages/import/Import.tsx
@@ -3,12 +3,14 @@ import Query from './components/query/Query';
 import QueryResults from './components/queryResults/QueryResults';
 
 import './import.css';
+import Footer from './components/Footer/Footer';
 
 export default function Import() {
 	return (
 		<div className="fade-in import" data-test="import">
 			<Query />
 			<QueryResults />
+			<Footer />
 		</div>
 	);
 }
