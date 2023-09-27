b74398da96c4cec38649b3ac179c5ced2078ed44
urecha <70019423+urecha@users.noreply.github.com>
2023-09-27 16:58:57 +0200

    refactor MiroImporter.tsx progress bar
    ProgressBar doesn't quite work with Lucid that well
    replaced it with an indefinite spinner

--- editorextensions/codebeamer-custom-ui/modal/src/pages/import/components/importer/MiroImporter.tsx
+++ editorextensions/codebeamer-custom-ui/modal/src/pages/import/components/importer/MiroImporter.tsx
@@ -3,7 +3,11 @@ import Modal from 'react-bootstrap/Modal';
 import ProgressBar from 'react-bootstrap/ProgressBar';
 import Spinner from 'react-bootstrap/Spinner';
 import { useGetItemsQuery } from '../../../../api/codeBeamerApi';
-import { closeModal, createAppCard, startImport } from '../../../../api/lucidGateway';
+import {
+	closeModal,
+	createAppCard,
+	startImport,
+} from '../../../../api/lucidGateway';
 import {
 	DEFAULT_RESULT_PAGE,
 	MAX_ITEMS_PER_IMPORT,
@@ -17,8 +21,6 @@ export default function Importer(props: {
 	items: CompressedItem[];
 	onClose?: Function;
 }) {
-	const [loaded, setLoaded] = useState(0);
-
 	/**
 	 * Produces the query string for the import.
 	 */
@@ -34,8 +36,8 @@ export default function Importer(props: {
 
 	React.useEffect(() => {
 		const importItems = async (items: CodeBeamerItem[]) => {
-            const importId = Math.ceil(Math.random() * 899) + 100;
-            startImport(importId, items.length);
+			const importId = Math.ceil(Math.random() * 899) + 100;
+			startImport(importId, items.length);
 			const _items: CodeBeamerItem[] = structuredClone(items);
 			for (let i = 0; i < _items.length; i++) {
 				console.log('Item no. ' + i + ' of ' + _items.length);
@@ -52,13 +54,12 @@ export default function Importer(props: {
 					}
 				}
 				await createAppCard(
-                    importId,
+					importId,
 					_items[i],
 					props.items.find(
 						(m) => m.id == _items[i].id.toString().trim()
 					)?.coordinates
 				);
-				setLoaded(i + 1);
 			}
 		};
 
@@ -82,19 +83,21 @@ export default function Importer(props: {
 			<Modal.Body>
 				<div className="centered w-80">
 					<h5 className="h5 text-center">
-						<Spinner animation="grow" variant="primary" />
-						<br />
-						{isLoading && <span>Fetching data</span>}
-						{!isLoading && <span>Creating cards</span>}
+						{isLoading && (
+							<>
+								<Spinner animation="grow" variant="primary" />
+								<br />
+								<span>Fetching data</span>
+							</>
+						)}
+						{!isLoading && (
+							<>
+								<Spinner animation="border" variant="primary" />
+								<br />
+								<span>Creating cards</span>
+							</>
+						)}
 					</h5>
-					<ProgressBar
-						className="w-100"
-						variant="primary"
-						now={loaded}
-						max={data?.total ?? props.items.length}
-						label={`${loaded}/${data?.total ?? props.items.length}`}
-						data-test="importProgress"
-					/>
 				</div>
 			</Modal.Body>
 		</Modal>
