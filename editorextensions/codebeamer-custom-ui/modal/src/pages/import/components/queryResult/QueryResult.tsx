8743d88c2ad7c1ef8ebf5a276efd813e3ebf294f
C2H6Ethan <ejbaum@live.com>
2023-10-18 09:37:49 +0200

    Added hover and made entire result clickable

--- editorextensions/codebeamer-custom-ui/modal/src/pages/import/components/queryResult/QueryResult.tsx
+++ editorextensions/codebeamer-custom-ui/modal/src/pages/import/components/queryResult/QueryResult.tsx
@@ -1,5 +1,6 @@
 import React, { useRef } from 'react';
 import { ItemListView } from '../../../../models/itemListView';
+import './queryResult.css';
 
 export default function QueryResult(props: {
 	item: ItemListView;
@@ -15,19 +16,31 @@ export default function QueryResult(props: {
 		}
 	}, []);
 
+	const handleCheckboxClick = (e: React.MouseEvent<HTMLInputElement>) => {
+		// Prevent event propagation to the parent tr element
+		e.stopPropagation();
+	};
+
+	const toggleCheckbox = () => {
+		if (checkbox.current) {
+			checkbox.current.click();
+		}
+	};
+
 	return (
-		<tr>
+		<tr className="queryResult" onClick={toggleCheckbox}>
 			<td>
 				<input
 					type="checkbox"
-					className="checkBox"
+					className="checkBox clickable"
 					onChange={(e) =>
 						props.onSelect(props.item, e.target.checked)
 					}
 					disabled={props.disabled}
 					data-test={'itemCheck-' + props.item.id}
 					ref={checkbox}
-				></input>
+					onClick={handleCheckboxClick}
+				/>
 			</td>
 			<td data-test="itemId">{props.item.id}</td>
 			<td data-test="itemName">{props.item.name}</td>
