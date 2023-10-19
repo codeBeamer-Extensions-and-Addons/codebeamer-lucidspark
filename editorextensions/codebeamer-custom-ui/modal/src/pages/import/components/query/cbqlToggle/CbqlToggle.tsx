1b1861b2f71e43c9a96a1d3e995de60cdace7b58
C2H6Ethan <ejbaum@live.com>
2023-10-19 11:26:29 +0200

    added a border before cbqltoggle

--- editorextensions/codebeamer-custom-ui/modal/src/pages/import/components/query/cbqlToggle/CbqlToggle.tsx
+++ editorextensions/codebeamer-custom-ui/modal/src/pages/import/components/query/cbqlToggle/CbqlToggle.tsx
@@ -15,38 +15,42 @@ export default function CbqlToggle() {
 	};
 
 	return (
-		<DefaultOverlayTrigger
-			content={advancedSearch ? 'CBQL Input' : 'Assisted Query'}
-		>
-			<button
-				className={'mx-1 mt-1 button button-secondary button-small'}
-				onClick={toggleSearchMethod}
-				data-test="search-method"
+		<div className="flex-row">
+			<div className="ml-1" />
+			<div className="mt-1 mr-1 soft-border-left" />
+			<DefaultOverlayTrigger
+				content={advancedSearch ? 'CBQL Input' : 'Assisted Query'}
 			>
-				{advancedSearch ? (
-					<i data-test="cbql-icon">
-						<svg
-							width="24"
-							height="24"
-							viewBox="0 0 28 28"
-							fill="none"
-							xmlns="http://www.w3.org/2000/svg"
-						>
-							<text fill="#000000">
-								<tspan x="4" y="13" textLength="20">
-									CB
-								</tspan>
-								<tspan x="4" y="25">
-									QL
-								</tspan>
-							</text>
-						</svg>
-					</i>
-				) : (
-					<span className="icon icon-parameters"></span>
-				)}
-			</button>
-		</DefaultOverlayTrigger>
+				<button
+					className={'mx-1 mt-1 button button-secondary button-small'}
+					onClick={toggleSearchMethod}
+					data-test="search-method"
+				>
+					{advancedSearch ? (
+						<i data-test="cbql-icon">
+							<svg
+								width="24"
+								height="24"
+								viewBox="0 0 28 28"
+								fill="none"
+								xmlns="http://www.w3.org/2000/svg"
+							>
+								<text fill="#000000">
+									<tspan x="4" y="13" textLength="20">
+										CB
+									</tspan>
+									<tspan x="4" y="25">
+										QL
+									</tspan>
+								</text>
+							</svg>
+						</i>
+					) : (
+						<span className="icon icon-parameters"></span>
+					)}
+				</button>
+			</DefaultOverlayTrigger>
+		</div>
 	);
 }
 
