c9905e51546d6149d9da844d48c76f2b2bb712ce
C2H6Ethan <ejbaum@live.com>
2023-11-02 13:53:24 +0100

    Add DefaultOverlayTrigger to ImportAction buttons

--- editorextensions/codebeamer-custom-ui/modal/src/pages/import/components/importActions/ImportActions.tsx
+++ editorextensions/codebeamer-custom-ui/modal/src/pages/import/components/importActions/ImportActions.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import './importActions.css';
+import DefaultOverlayTrigger from '../../../../components/defaultOverlayTrigger/DefautOverlayTrigger';
 
 export default function ImportActions(props: {
 	selectedCount: number;
@@ -16,68 +17,92 @@ export default function ImportActions(props: {
 }) {
 	return (
 		<div className="w-100 flex-row">
-			<button
-				className="button button-primary button-small flex flex-centered"
-				onClick={() => props.onImportSelected()}
-				disabled={props.selectedCount == 0}
-				data-test="importSelected"
+			<DefaultOverlayTrigger
+				content={`Import selected items (${props.selectedCount})`}
 			>
-				<span className="mr-5p">
-					<svg
-						width="24"
-						height="24"
-						fill="none"
-						xmlns="http://www.w3.org/2000/svg"
-					>
-						<path
-							d="M11 12.172L8.707 9.879a1 1 0 00-1.414 1.414L12 16l4.707-4.707a1 1 0 00-1.414-1.414L13 12.172V2.586a1 1 0 10-2 0v9.586zM3 19a1 1 0 100 2h18a1 1 0 100-2H3z"
-							fill="#fff"
-						/>
-					</svg>
-				</span>
-				Import Selected ({props.selectedCount})
-			</button>
-			<button
-				className="button button-primary button-small flex flex-centered"
-				disabled={props.unImportedItemsCount == 0}
-				onClick={() => props.onImportAll()}
-				data-test="importAll"
+				<button
+					className="button button-primary button-small flex flex-centered"
+					onClick={() => props.onImportSelected()}
+					disabled={props.selectedCount == 0}
+					data-test="importSelected"
+				>
+					<span className="mr-5p">
+						<svg
+							width="24"
+							height="24"
+							fill="none"
+							xmlns="http://www.w3.org/2000/svg"
+						>
+							<path
+								d="M11 12.172L8.707 9.879a1 1 0 00-1.414 1.414L12 16l4.707-4.707a1 1 0 00-1.414-1.414L13 12.172V2.586a1 1 0 10-2 0v9.586zM3 19a1 1 0 100 2h18a1 1 0 100-2H3z"
+								fill="#fff"
+							/>
+						</svg>
+					</span>
+					Import Selected ({props.selectedCount})
+				</button>
+			</DefaultOverlayTrigger>
+
+			<DefaultOverlayTrigger
+				content={`Import all remaining items (${props.unImportedItemsCount})`}
 			>
-				<span className="mr-5p">
-					<svg
-						width="24"
-						height="24"
-						fill="none"
-						xmlns="http://www.w3.org/2000/svg"
-					>
-						<path
-							d="M11 12.172L8.707 9.879a1 1 0 00-1.414 1.414L12 16l4.707-4.707a1 1 0 00-1.414-1.414L13 12.172V2.586a1 1 0 10-2 0v9.586zM3 19a1 1 0 100 2h18a1 1 0 100-2H3z"
-							fill="#fff"
-						/>
-					</svg>
-				</span>
-				Import all ({props.unImportedItemsCount})
-			</button>
-			<button
-				className="button button-secondary button-small flex flex-centered"
-				disabled={props.importedItemsCount == 0}
-				data-test="sync"
-				onClick={() => props.onSync()}
+				<button
+					className="button button-primary button-small flex flex-centered"
+					disabled={props.unImportedItemsCount == 0}
+					onClick={() => props.onImportAll()}
+					data-test="importAll"
+				>
+					<span className="mr-5p">
+						<svg
+							width="24"
+							height="24"
+							fill="none"
+							xmlns="http://www.w3.org/2000/svg"
+						>
+							<path
+								d="M11 12.172L8.707 9.879a1 1 0 00-1.414 1.414L12 16l4.707-4.707a1 1 0 00-1.414-1.414L13 12.172V2.586a1 1 0 10-2 0v9.586zM3 19a1 1 0 100 2h18a1 1 0 100-2H3z"
+								fill="#fff"
+							/>
+						</svg>
+					</span>
+					Import all ({props.unImportedItemsCount})
+				</button>
+			</DefaultOverlayTrigger>
+
+			<DefaultOverlayTrigger
+				content={`Sync Cards with Codebeamer (${props.importedItemsCount})`}
 			>
-				<span className="icon icon-refresh mr-1 pos-adjusted-down"></span>
-				Sync ({props.importedItemsCount})
-			</button>
-			<button
-				className="button button-secondary button-small flex flex-centered"
-				disabled={props.relationsCount == 0}
-				data-test="relations"
-				onClick={() => props.onRelations()}
+				<button
+					className="button button-secondary button-small flex flex-centered"
+					disabled={props.importedItemsCount == 0}
+					data-test="sync"
+					onClick={() => props.onSync()}
+				>
+					<span className="icon icon-refresh mr-1 pos-adjusted-down clickable"></span>
+					Sync ({props.importedItemsCount})
+				</button>
+			</DefaultOverlayTrigger>
+
+			<DefaultOverlayTrigger
+				content={
+					props.relationsCount > 0 && props.missingRelationsCount == 0
+						? `Hide Relations & Associations (${props.relationsCount})`
+						: `Show Relations & Associations (${props.missingRelationsCount})`
+				}
 			>
-				<span className="icon icon-arrow-line-shape mr-1 pos-adjusted-down"></span>
-				{props.relationsCount > 0 && props.missingRelationsCount == 0
-					? `Hide Relations & Associations (${props.relationsCount})`
-					: `Show Relations & Associations (${props.missingRelationsCount})`}
-			</button>
+				<button
+					className="button button-secondary button-small flex flex-centered"
+					disabled={props.relationsCount == 0}
+					data-test="relations"
+					onClick={() => props.onRelations()}
+				>
+					<span className="icon icon-arrow-line-shape mr-1 pos-adjusted-down clickable"></span>
+					{props.relationsCount > 0 &&
+					props.missingRelationsCount == 0
+						? `Hide Relations (${props.relationsCount})`
+						: `Show Relations (${props.missingRelationsCount})`}
+				</button>
+			</DefaultOverlayTrigger>
 		</div>
 	);
 }
