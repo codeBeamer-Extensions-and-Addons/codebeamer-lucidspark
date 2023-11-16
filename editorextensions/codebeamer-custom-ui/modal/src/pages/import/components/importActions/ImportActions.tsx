6faaec9a128eb18c9ae9a5e2dd5e111d0ddbf1b5
urecha <70019423+urecha@users.noreply.github.com>
2023-11-16 07:21:36 +0100

    minor refactorings ImportActions.tsx
    - Format file
    - Remove tooltip from "Import Selected" button (obsolete)
    - Rephrase most tooltips & button texts
    Minimize & generalize words
    - Remove duplicate information (count in both button & tooltip)
    - Pull spinner in relations-button up a level
    shan't spin next to the static icon
    - Set relations icon to go downards to symbolize removing

--- editorextensions/codebeamer-custom-ui/modal/src/pages/import/components/importActions/ImportActions.tsx
+++ editorextensions/codebeamer-custom-ui/modal/src/pages/import/components/importActions/ImportActions.tsx
@@ -1,7 +1,7 @@
-import React from "react";
-import Spinner from "react-bootstrap/Spinner";
-import "./importActions.css";
-import DefaultOverlayTrigger from "../../../../components/defaultOverlayTrigger/DefautOverlayTrigger";
+import React from 'react';
+import Spinner from 'react-bootstrap/Spinner';
+import './importActions.css';
+import DefaultOverlayTrigger from '../../../../components/defaultOverlayTrigger/DefautOverlayTrigger';
 
 export default function ImportActions(props: {
 	selectedCount: number;
@@ -19,35 +19,30 @@ export default function ImportActions(props: {
 }) {
 	return (
 		<div className="w-100 flex-row">
-			<DefaultOverlayTrigger
-				content={`Import selected items (${props.selectedCount})`}
-				placement="top"
+			<button
+				className="button button-primary button-small flex flex-centered"
+				onClick={() => props.onImportSelected()}
+				disabled={props.selectedCount == 0}
+				data-test="importSelected"
 			>
-				<button
-					className="button button-primary button-small flex flex-centered"
-					onClick={() => props.onImportSelected()}
-					disabled={props.selectedCount == 0}
-					data-test="importSelected"
-				>
-					<span className="mr-5p">
-						<svg
-							width="24"
-							height="24"
-							fill="none"
-							xmlns="http://www.w3.org/2000/svg"
-						>
-							<path
-								d="M11 12.172L8.707 9.879a1 1 0 00-1.414 1.414L12 16l4.707-4.707a1 1 0 00-1.414-1.414L13 12.172V2.586a1 1 0 10-2 0v9.586zM3 19a1 1 0 100 2h18a1 1 0 100-2H3z"
-								fill="#fff"
-							/>
-						</svg>
-					</span>
-					Import Selected ({props.selectedCount})
-				</button>
-			</DefaultOverlayTrigger>
+				<span className="mr-5p">
+					<svg
+						width="24"
+						height="24"
+						fill="none"
+						xmlns="http://www.w3.org/2000/svg"
+					>
+						<path
+							d="M11 12.172L8.707 9.879a1 1 0 00-1.414 1.414L12 16l4.707-4.707a1 1 0 00-1.414-1.414L13 12.172V2.586a1 1 0 10-2 0v9.586zM3 19a1 1 0 100 2h18a1 1 0 100-2H3z"
+							fill="#fff"
+						/>
+					</svg>
+				</span>
+				Import selection ({props.selectedCount})
+			</button>
 
 			<DefaultOverlayTrigger
-				content={`Import all remaining items (${props.unImportedItemsCount})`}
+				content={`Import all not yet imported items in the view`}
 				placement="top"
 			>
 				<button
@@ -74,7 +69,7 @@ export default function ImportActions(props: {
 			</DefaultOverlayTrigger>
 
 			<DefaultOverlayTrigger
-				content={`Sync Cards with Codebeamer (${props.importedItemsCount})`}
+				content={`Sync imported cards with codebeamer`}
 				placement="top"
 			>
 				<button
@@ -83,7 +78,7 @@ export default function ImportActions(props: {
 					data-test="sync"
 					onClick={() => props.onSync()}
 				>
-					<span className="icon icon-refresh mr-1 pos-adjusted-down clickable"></span>
+					<span className="icon icon-refresh mr-25 pos-adjusted-down clickable"></span>
 					({props.importedItemsCount})
 				</button>
 			</DefaultOverlayTrigger>
@@ -93,9 +88,9 @@ export default function ImportActions(props: {
 					props.areAllRelationsLoaded
 						? props.relationsCount > 0 &&
 						  props.missingRelationsCount === 0
-							? `Hide Relations & Associations (${props.relationsCount})`
-							: `Show Relations & Associations (${props.missingRelationsCount})`
-						: "Toggle Relations & Associations"
+							? `Hide relations for items in the tracker`
+							: `Show relations for items in the tracker`
+						: 'Toggle relations for items in the Tracker'
 				}
 				placement="top"
 			>
@@ -103,19 +98,33 @@ export default function ImportActions(props: {
 					className="button button-secondary button-small flex flex-centered"
 					disabled={
 						props.isRelationsLoading ||
-						(props.areAllRelationsLoaded && props.relationsCount === 0)
+						(props.areAllRelationsLoaded &&
+							props.relationsCount === 0)
 					}
 					data-test="relations"
 					onClick={() => props.onRelations()}
 				>
-					<span
-						className={`icon icon-arrow-line-shape ${
-							props.areAllRelationsLoaded ? "mr-1" : ""
-						} pos-adjusted-down clickable`}
-					></span>
-
 					{props.isRelationsLoading ? (
-						<Spinner animation="border" variant="secondary" />
+						<Spinner
+							animation="border"
+							variant="secondary"
+							size="sm"
+						/>
+					) : 
+						<span
+							className={`icon icon-arrow-line-shape ${
+								props.areAllRelationsLoaded ? 'mr-25' : ''
+							} ${
+								props.areAllRelationsLoaded &&
+								props.relationsCount > 0 &&
+								props.missingRelationsCount == 0
+									? 'rotate-90'
+									: ''
+							} pos-adjusted-down clickable`}
+						></span>
+					}
+					{props.isRelationsLoading ? (
+						<></>
 					) : props.areAllRelationsLoaded ? (
 						props.relationsCount > 0 &&
 						props.missingRelationsCount === 0 ? (
